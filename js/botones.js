94b9677a0e6556dcf6561f44c65fbeceabe871d0
Hachiro83 <45296762+Hachiro83@users.noreply.github.com>
2018-11-23 20:07:31 -0600

    homepage preview

---
+++ js/botones.js
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////
+//boton_article_1
+/////////////////////////////////////////////////////////
+function show_articel_1() {
+	 //alert("I am an alert box!");
+	 $('#article_1').removeAttr('style');
+	 $('#boton_article_1').attr('style', 'display:none');
+
+}
+
+function hide_articel_1() {
+	 //alert("I am an alert box!");
+	 $('#article_1').attr('style', 'display:none');
+	 $('#boton_article_1').removeAttr('style');
+
+}
+/////////////////////////////////////////////////////////
+//boton_article_2
+/////////////////////////////////////////////////////////
+function show_articel_2() {
+	 //alert("I am an alert box!");
+	 $('#article_2').removeAttr('style');
+	 $('#boton_article_2').attr('style', 'display:none');
+
+}
+
+function hide_articel_2() {
+	 //alert("I am an alert box!");
+	 $('#article_2').attr('style', 'display:none');
+	 $('#boton_article_2').removeAttr('style');
+
+}
+
+/////////////////////////////////////////////////////////
+//boton_article_3
+/////////////////////////////////////////////////////////
+function show_articel_3() {
+	 //alert("I am an alert box!");
+	 $('#article_3').removeAttr('style');
+	 $('#boton_article_3').attr('style', 'display:none');
+
+}
+
+function hide_articel_3() {
+	 //alert("I am an alert box!");
+	 $('#article_3').attr('style', 'display:none');
+	 $('#boton_article_3').removeAttr('style');
+
+}
